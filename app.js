6ce89cd80d34801622d5a6c94648575e55ce68fb
daisykumar <56020145+daisykumar@users.noreply.github.com>
2021-01-25 12:32:08 +0100

    JS

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCG-EE0kPyk3S_lEqQ93hjq-6Z6kPwXgsc",
-    authDomain: "daisy-kumar-portfolio.firebaseapp.com",
-    projectId: "daisy-kumar-portfolio",
-    storageBucket: "daisy-kumar-portfolio.appspot.com",
-    messagingSenderId: "704513711349",
-    appId: "1:704513711349:web:5656717463a935e76439bc"
-};
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
