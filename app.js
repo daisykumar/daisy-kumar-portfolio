132d7f5fd48ad355dd48572c80065b0594652442
daisykumar <56020145+daisykumar@users.noreply.github.com>
2021-02-06 23:33:08 +0100

    update

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 const social = document.querySelector('.social-icons');
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
 
 
 let container = document.querySelector('#background-container');
@@ -82,22 +80,6 @@ app.content.get({
   container.innerHTML = html
 })
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-  // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
 navToggle.addEventListener("click", function (){
     links.classList.toggle("show-links");
     links.classList.toggle("show-socials");
--- project-page.js
+++ project-page.js
@@ -3,6 +3,26 @@ let projectContainer = document.querySelector('#project');
 const queryString = window.location.search; // "?id=asdfasdfasdf"
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 if(id) {
     app.content.get({
